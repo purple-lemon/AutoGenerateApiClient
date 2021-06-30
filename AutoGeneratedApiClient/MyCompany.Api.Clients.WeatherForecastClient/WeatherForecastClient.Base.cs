bc01ba4d750a6c82c8ebd8333c86acdb79aba0d8
Bogdan <byurch@softserveinc.com>
2021-06-30 13:17:37 +0300

    Initial commit

---
+++ AutoGeneratedApiClient/MyCompany.Api.Clients.WeatherForecastClient/WeatherForecastClient.Base.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCompany.Api.Clients.WeatherForecastClient
+{
+	public partial class WeatherForecastClient
+	{
+		public string BaseUrl { get; set; }
+	}
+}
---
+++ AutoGeneratedApiClient/WeatherApiConsoleCheck/Program.cs
@@ -0,0 +1,24 @@
+﻿using MyCompany.Api.Clients.WeatherForecastClient;
+using System;
+using System.Threading.Tasks;
+
+namespace WeatherApiConsoleCheck
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			Console.WriteLine("Presss enter to start");
+			Console.ReadLine();
+			IWeatherForecastClient apiClient = new WeatherForecastClient("http://localhost:5000");
+			var forecasts = await apiClient.GetAllAsync();
+
+			var resultOfAdding = await apiClient.AddAsync(new AddWeatherData()
+			{
+				Kind = TemperatureType.Celcium,
+				Region = "Ukraine",
+				Temperature = 50
+			});
+		}
+	}
+}
