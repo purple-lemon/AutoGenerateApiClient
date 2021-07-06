14e360405942b22e6386cf5739c7ce0459e20fd8
Bogdan <byurch@softserveinc.com>
2021-07-06 11:59:49 +0300

    Several simple things for demo

--- AutoGeneratedApiClient/WeatherApiConsoleCheck/Program.cs
+++ AutoGeneratedApiClient/WeatherApiConsoleCheck/Program.cs
@@ -8,17 +8,42 @@ namespace WeatherApiConsoleCheck
 	{
 		static async Task Main(string[] args)
 		{
+			var baseUrl = "http://localhost:5000";
 			Console.WriteLine("Presss enter to start");
 			Console.ReadLine();
-			IWeatherForecastClient apiClient = new WeatherForecastClient("http://localhost:5000");
+			// Interface can be resolved in API clients over DI
+			IWeatherForecastClient apiClient = new WeatherForecastClient(baseUrl);
+
+			// Call to get data
 			var forecasts = await apiClient.GetAllAsync();
+
+			// set username and pass
 			apiClient.UseUserAuth("admin", "321123");
+
+
+			// add data to api closed with AUTH
 			var resultOfAdding = await apiClient.AddAsync(new AddWeatherData()
 			{
 				Kind = TemperatureType.Celcium,
 				Region = "Ukraine",
 				Temperature = 50
 			});
+			Console.WriteLine($"Is succeffully added: {resultOfAdding}");
+
+			// Example that another controller also has client:
+			var authClient = new AuthClient(baseUrl);
+			try
+			{
+				var token = await authClient.LoginAsync("", new LoginModel()
+				{
+					UserName = "HasNoAccess",
+					Password = "11111"
+				});
+			} catch (SwaggerException e){
+				Console.WriteLine("Unauthorized");
+			}
+
+			Console.ReadLine();
 		}
 	}
 }
